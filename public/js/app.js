f62d28082da96a61eb152e1fc0172ca4d771504a
John <hov86@yahoo.com>
2019-09-29 23:38:14 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+
+
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('input')
+const messageOne = document.querySelector('.message-1')
+const messageTwo = document.querySelector('.message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = searchInput.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
+
+weatherForm.addEventListener('change', (e) => {
+    e.preventDefault()
+    searchInput.textContent = ''
+    messageOne.textContent = ''
+    messageTwo.textContent = ''
+})
